b457b6abf9ef8930b4219fd9bc208430e12adf61
MauricioSuilice19 <lucasuilice@hotmail.com.ar>
2026-05-25 00:16:49 -0300

    corregir

--- tp-webservice/tp-webservice/src/app/services/qr.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class QrService {
-  generarUrlQr(texto: string): string {
-    return `https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=${encodeURIComponent(texto.trim())}`;
-  }
-}
